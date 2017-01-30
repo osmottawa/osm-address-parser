4fee0f182d5412dd103545c0fbf0f9ab60802d40
Denis <carriere.denis@gmail.com>
2017-01-30 12:25:32 -0500

    Move test cases to `test/in`

--- test.js
+++ test.js
@@ -1,26 +1,26 @@
-const osmify = require('.')
+const fs = require('fs')
 const path = require('path')
-const read = require('.').read
-const save = require('.').save
+const osmify = require('.')
 
 const directories = {
-  out: path.join(__dirname, 'test', 'fixtures', 'out'),
-  in: path.join(__dirname, 'test', 'fixtures', 'in')
+  out: path.join(__dirname, 'fixtures', 'out'),
+  in: path.join(__dirname, 'fixtures', 'in')
 }
 
-const fixtures = {
-  address: {
-    in: path.join(directories.in, 'address.json'),
-    out: path.join(directories.out, 'address.json')
-  }
+function save (directory, filename, features) {
+  return fs.writeFileSync(path.join(directory, filename), JSON.stringify(features, null, 2))
 }
 
-describe('parse', () => {
+function read (directory, filename) {
+  return JSON.parse(fs.readFileSync(path.join(directory, filename), 'utf8'))
+}
+
+describe('osmify', () => {
   test('address', () => {
-    const address = read(fixtures.address.in)
-    const data = osmify.parse(address)
-    if (process.env.REGEN) { save(fixtures.address.out, data) }
-    expect(data, read(fixtures.address.out))
+    const address = read(directories.in, 'address.json')
+    const data = osmify(address)
+    if (process.env.REGEN) { save(directories.out, 'address.json', data) }
+    expect(data, read(directories.out, 'address.json'))
   })
 })
 
